a6507f6aa0dc1a7747c3c7f1d6304dc22c140568
Mariana <marianaufc@gmail.com>
2023-02-27 10:42:38 -0300

    feat(rentalx): loads uploaded file into repository

--- p3-rentalx/src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
+++ p3-rentalx/src/modules/cars/useCases/importCategory/importCategoryUseCase.ts
@@ -1,14 +1,43 @@
 import { parse } from "csv-parse";
 import fs from "fs";
 
+import { type ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategories {
+    name: string;
+    description: string;
+}
 class ImportCategoryUseCase {
-    execute(file: Express.Multer.File): void {
-        const stream = fs.createReadStream(file.path);
-        const parseFile = parse();
-        stream.pipe(parseFile);
-        parseFile.on("data", (line) => {
-            console.log(line);
+    constructor(private readonly categoriesRepository: ICategoriesRepository) {}
+
+    async loadCategories(
+        file: Express.Multer.File
+    ): Promise<IImportCategories[]> {
+        return await new Promise((resolve, reject) => {
+            const categories: IImportCategories[] = [];
+            const stream = fs.createReadStream(file.path);
+            const parseFile = parse();
+            stream.pipe(parseFile);
+            parseFile
+                .on("data", async (line) => {
+                    const [name, description] = line;
+                    categories.push({
+                        name,
+                        description,
+                    });
+                })
+                .on("end", () => {
+                    resolve(categories);
+                })
+                .on("error", (err) => {
+                    reject(err);
+                });
         });
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+        console.log(categories);
     }
 }
 
