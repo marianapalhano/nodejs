59b5914654f5e1b2e782b21e8de15ba7d8fb48e4
Mariana <marianaufc@gmail.com>
2023-03-10 15:54:46 -0300

    feat(rentalx): adds tsyringe and categories repository dependency injection

--- p3-rentalx/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ p3-rentalx/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,16 +1,16 @@
 import { type Request, type Response } from "express";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { container } from "tsyringe";
 
-import { type CreateCategoryUseCase } from "./CreateCategoryUseCase";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoryController {
-    constructor(
-        private readonly createCategoryUseCase: CreateCategoryUseCase
-    ) {}
-
     async handle(request: Request, response: Response): Promise<Response> {
         const { name, description } = request.body;
 
-        await this.createCategoryUseCase.execute({ name, description });
+        const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
+
+        await createCategoryUseCase.execute({ name, description });
         return response.status(201).send();
     }
 }
--- p3-rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ p3-rentalx/src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { inject, injectable } from "tsyringe";
+
 import { type ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
@@ -5,8 +8,13 @@ interface IRequest {
     description: string;
 }
 
+@injectable()
 class CreateCategoryUseCase {
-    constructor(private readonly categoriesRepository: ICategoriesRepository) {}
+    constructor(
+        @inject("CategoriesRepository")
+        private readonly categoriesRepository: ICategoriesRepository
+    ) {}
+
     async execute({ name, description }: IRequest): Promise<void> {
         const categoryAlreadyExists =
             await this.categoriesRepository.findByName(name);
