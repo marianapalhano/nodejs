6af4d973a21f3855be2dad757cc96900e3e88eb8
Mariana <marianaufc@gmail.com>
2023-03-19 10:14:56 -0300

    feat(rentalx): adds jwt lib and authenticate user use case

---
+++ p3-rentalx/src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,53 @@
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IReturn {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private readonly usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ email, password }: IRequest): Promise<IReturn> {
+        const user = await this.usersRepository.findByEmail(email);
+        if (user == null) {
+            throw new Error("Incorrect email or password");
+        }
+
+        const paswordMatches = await compare(password, user.password);
+        if (!paswordMatches) {
+            throw new Error("Incorrect email or password");
+        }
+
+        const token = sign({}, "c7025305a85ddae9c5fd1a8cd9168f89", {
+            subject: user.id,
+            expiresIn: "1d",
+        });
+
+        return {
+            user: {
+                name: user.name,
+                email: user.email,
+            },
+            token,
+        };
+    }
+}
+
+export { AuthenticateUserUseCase };
