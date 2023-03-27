abe6616a277e526eb9db08306454644dc6651e29
Mariana <marianaufc@gmail.com>
2023-03-27 16:19:17 -0300

    feat(rentalx): creates categories repository in memory

---
+++ p3-rentalx/src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,31 @@
+import { Category } from "../../entities/Category";
+import {
+    type ICategoriesRepository,
+    type ICategory,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+    categories: Category[] = [];
+
+    async findByName(name: string): Promise<Category | null> {
+        const category = this.categories.find(
+            (category) => category.name === name
+        );
+        return category;
+    }
+
+    async list(): Promise<Category[]> {
+        return this.categories;
+    }
+
+    async create({ name, description }: ICategory): Promise<void> {
+        const category = new Category();
+        Object.assign(category, {
+            name,
+            description,
+        });
+        this.categories.push(category);
+    }
+}
+
+export { CategoriesRepositoryInMemory };
