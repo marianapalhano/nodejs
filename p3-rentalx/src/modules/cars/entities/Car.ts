2aa50b70865ae66098bf48863b825e3018435197
Mariana <marianaufc@gmail.com>
2023-04-03 11:57:42 -0300

    feat(rentalx): creates car entity

---
+++ p3-rentalx/src/modules/cars/entities/Car.ts
@@ -0,0 +1,69 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    PrimaryColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "./Category";
+import { Specification } from "./Specification";
+
+@Entity("cars")
+class Car {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    daily_fee: number;
+
+    @Column()
+    is_available: boolean;
+
+    @Column()
+    license_plate: string;
+
+    @Column()
+    fine_amount: number;
+
+    @Column()
+    carmaker: string;
+
+    @ManyToOne(() => Category)
+    @JoinColumn({ name: "category_id" })
+    category: Category;
+
+    @Column()
+    category_id: string;
+
+    @ManyToMany(() => Specification)
+    @JoinTable({
+        name: "specifications_cars",
+        joinColumns: [{ name: "car_id" }],
+        inverseJoinColumns: [{ name: "specification_id" }],
+    })
+    specifications: Specification[];
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+            this.is_available = true;
+        }
+    }
+}
+
+export { Car };
