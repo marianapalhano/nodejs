2a6b0716aedad337ee8b7b7ef69237e9f7b6922b
Mariana <marianaufc@gmail.com>
2023-04-05 14:29:38 -0300

    feat(rentalx): adds create car controller

---
+++ p3-rentalx/src/modules/cars/useCases/createCar/CreateCarController.ts
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+import { type Request, type Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateCarUseCase } from "./CreateCarUseCase";
+
+class CreateCarController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const {
+            name,
+            description,
+            license_plate,
+            daily_fee,
+            fine_amount,
+            carmaker,
+            category_id,
+        } = request.body;
+        const createCarUseCase = container.resolve(CreateCarUseCase);
+        const car = await createCarUseCase.execute({
+            name,
+            description,
+            license_plate,
+            daily_fee,
+            fine_amount,
+            carmaker,
+            category_id,
+        });
+        return response.status(201).json(car);
+    }
+}
+
+export { CreateCarController };
--- p3-rentalx/src/shared/container/index.ts
+++ p3-rentalx/src/shared/container/index.ts
@@ -3,7 +3,9 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/repositories/implementations/UsersRepository";
 import { type IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { type ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { type ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
+import { CarsRepository } from "@modules/cars/repositories/implementations/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/repositories/implementations/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/repositories/implementations/SpecificationsRepository";
 import { type ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
@@ -22,3 +24,5 @@ container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
