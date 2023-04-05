a3852eda260da8c4ae4bc3446ecb529b41ddbdee
Mariana <marianaufc@gmail.com>
2023-04-05 14:35:02 -0300

    feat(rentalx): adds cars routes

---
+++ p3-rentalx/src/routes/cars.routes.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import { Router } from "express";
+
+import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+
+const carsRoutes = Router();
+
+const createCarController = new CreateCarController();
+
+carsRoutes.post("/", createCarController.handle);
+
+export { carsRoutes };
