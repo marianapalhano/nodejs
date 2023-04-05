9879fed572b50e906c1e5620e7ac05dbb04b2d2b
Mariana <marianaufc@gmail.com>
2023-04-05 10:39:09 -0300

    feat(rentalx): implements create car use case tests

--- p3-rentalx/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ p3-rentalx/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-confusing-void-expression */
+import { AppError } from "@errors/AppError";
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 
 import { CreateCarUseCase } from "./CreateCarUseCase";
@@ -20,5 +22,43 @@ describe("Create Car", () => {
             carmaker: "Make of car",
             category_id: "1234",
         });
+    });
+
+    it("should not be able to create a car with a license plate that is already registered", async () => {
+        await createCarUseCase.execute({
+            name: "Car1",
+            description: "Car Description",
+            daily_fee: 99,
+            license_plate: "ABC1234",
+            fine_amount: 60,
+            carmaker: "Maker of car",
+            category_id: "category",
+        });
+
+        await expect(
+            createCarUseCase.execute({
+                name: "Car2",
+                description: "Description Car",
+                daily_fee: 99,
+                license_plate: "ABC1234",
+                fine_amount: 60,
+                carmaker: "Maker of car",
+                category_id: "category",
+            })
+        ).rejects.toEqual(new AppError("Car already exists"));
+    });
+
+    it("should not be able to create a car with is_available true by default", async () => {
+        const car = await createCarUseCase.execute({
+            name: "Car Available",
+            description: "Car Description",
+            daily_fee: 99,
+            license_plate: "ABCD1234",
+            fine_amount: 60,
+            carmaker: "Maker of car",
+            category_id: "category",
+        });
+
+        expect(car.is_available).toBe(true);
     });
 });
