06ad00e92aed1796f1f0b7515b04d47d83327273
Mariana <marianaufc@gmail.com>
2023-04-06 17:52:24 -0300

    feat(rentalx): adds list available cars to routes

--- p3-rentalx/src/routes/cars.routes.ts
+++ p3-rentalx/src/routes/cars.routes.ts
@@ -4,10 +4,12 @@ import { ensureAdmin } from "middlewares/ensureAdmin";
 import { ensureAuthenticated } from "middlewares/ensureAuthenticated";
 
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listCars/ListAvailableCarsController";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post(
     "/",
@@ -15,5 +17,7 @@ carsRoutes.post(
     ensureAdmin,
     createCarController.handle
 );
+
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes };
