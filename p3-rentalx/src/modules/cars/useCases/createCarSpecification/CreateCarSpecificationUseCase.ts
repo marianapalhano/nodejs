a03ab5a02994ba77b9240fffdf77657c5d4b40d4
Mariana <marianaufc@gmail.com>
2023-04-07 15:45:54 -0300

    feat(rentalx): adds create car specification use case

---
+++ p3-rentalx/src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationUseCase.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@errors/AppError";
+import { type Car } from "@modules/cars/entities/Car";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ISpecificationsRepository } from "@modules/cars/repositories/ISpecificationsRepository";
+
+interface IRequest {
+    car_id: string;
+    specifications_id: string[];
+}
+
+@injectable()
+class CreateCarSpecificationUseCase {
+    constructor(
+        @inject("CarsRepository")
+        private readonly carsRepository: ICarsRepository,
+
+        @inject("SpecificationsRepository")
+        private readonly specificationsRepository: ISpecificationsRepository
+    ) {}
+
+    async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
+        const carExists = await this.carsRepository.findById(car_id);
+
+        if (!carExists) {
+            throw new AppError("Car does not exist");
+        }
+
+        const specifications = await this.specificationsRepository.findByIds(
+            specifications_id
+        );
+
+        carExists.specifications = specifications;
+
+        await this.carsRepository.create(carExists);
+
+        return carExists;
+    }
+}
+
+export { CreateCarSpecificationUseCase };
