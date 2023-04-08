7456ffea1e92954594516e5ac8f1a516fffbae48
Mariana <marianaufc@gmail.com>
2023-04-08 08:25:29 -0300

    feat(rentalx): adds car image upload

--- p3-rentalx/src/routes/cars.routes.ts
+++ p3-rentalx/src/routes/cars.routes.ts
@@ -2,16 +2,22 @@
 import { Router } from "express";
 import { ensureAdmin } from "middlewares/ensureAdmin";
 import { ensureAuthenticated } from "middlewares/ensureAuthenticated";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listCars/ListAvailableCarsController";
+import { UploadCarImageController } from "@modules/cars/useCases/uploadCarImage/UploadCarImageController";
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImageController = new UploadCarImageController();
+
+const uploadCarImages = multer(uploadConfig.upload("./tmp/cars"));
 
 carsRoutes.post(
     "/",
@@ -27,6 +33,14 @@ carsRoutes.post(
     ensureAuthenticated,
     ensureAdmin,
     createCarSpecificationController.handle
+);
+
+carsRoutes.post(
+    "/images/:id",
+    ensureAuthenticated,
+    ensureAdmin,
+    uploadCarImages.array("images"),
+    uploadCarImageController.handle
 );
 
 export { carsRoutes };
