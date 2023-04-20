f56966f2a9b0a6e808cceb4a6fb50ea9c88d11a2
Mariana <marianaufc@gmail.com>
2023-04-20 09:51:38 -0300

    feat(rentalx): adds users tokens repository interface

---
+++ p3-rentalx/src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -0,0 +1,26 @@
+import { type UsersTokens } from "../entities/UsersTokens";
+
+interface ICreateUsersTokens {
+    user_id: string;
+    expires_date: Date;
+    refresh_token: string;
+}
+
+interface IUsersTokensRepository {
+    create: ({
+        expires_date,
+        refresh_token,
+        user_id,
+    }: ICreateUsersTokens) => Promise<UsersTokens>;
+
+    findByUserIdAndRefreshToken: (
+        user_id: string,
+        refresh_token: string
+    ) => Promise<UsersTokens>;
+
+    deleteById: (id: string) => Promise<void>;
+
+    findByRefreshToken: (refresh_token: string) => Promise<UsersTokens>;
+}
+
+export type { IUsersTokensRepository };
