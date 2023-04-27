a182e1646349909419be7107874cc062df7f40ce
Mariana <marianaufc@gmail.com>
2023-04-27 12:05:52 -0300

    feat(rentalx): adds reset password use case

---
+++ p3-rentalx/src/modules/accounts/useCases/resetPassword/resetPasswordUseCase.ts
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/no-throw-literal */
+import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@errors/AppError";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { IUsersTokensRepository } from "@modules/accounts/repositories/IUsersTokensRepository";
+import { IDateProvider } from "@shared/container/providers/DateProvider/IDateProvider";
+
+interface IRequest {
+    token: string;
+    password: string;
+}
+
+@injectable()
+class ResetPasswordUseCase {
+    constructor(
+        @inject("UsersTokensRepository")
+        private readonly usersTokensRepository: IUsersTokensRepository,
+        @inject("DayjsDateProvider")
+        private readonly dateProvider: IDateProvider,
+        @inject("UsersRepository")
+        private readonly usersRepository: IUsersRepository
+    ) {}
+
+    async execute({ token, password }: IRequest): Promise<void> {
+        const userToken = await this.usersTokensRepository.findByRefreshToken(
+            token
+        );
+
+        if (!userToken) {
+            throw new AppError("Token invalid!");
+        }
+
+        if (
+            this.dateProvider.compareIfBefore(
+                userToken.expires_date,
+                this.dateProvider.dateNow()
+            )
+        ) {
+            throw new AppError("Token expired!");
+        }
+
+        const user = await this.usersRepository.findById(userToken.user_id);
+
+        user.password = await hash(password, 8);
+
+        await this.usersRepository.create(user);
+
+        await this.usersTokensRepository.deleteById(userToken.id);
+    }
+}
+
+export { ResetPasswordUseCase };
