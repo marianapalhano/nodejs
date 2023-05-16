4b011d3591ddec26b01debf4524b8f7f3e44e046
Mariana <marianaufc@gmail.com>
2023-05-16 09:33:42 -0300

    feat(rentalx): adds cors

--- p3-rentalx/src/app.ts
+++ p3-rentalx/src/app.ts
@@ -3,6 +3,7 @@ import express, {
     type Request,
     type Response,
 } from "express";
+import cors from "cors";
 import "dotenv/config";
 import "express-async-errors";
 import "reflect-metadata";
@@ -39,6 +40,7 @@ app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 
+app.use(cors());
 app.use(router);
 
 app.use(
