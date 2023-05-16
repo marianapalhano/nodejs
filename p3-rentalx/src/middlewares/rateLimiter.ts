1f13e4d0745540b603f90a22b44321b3978e26e6
Mariana <marianaufc@gmail.com>
2023-05-16 09:02:51 -0300

    feat(rentalx): adds redis and rate-limiter

---
+++ p3-rentalx/src/middlewares/rateLimiter.ts
@@ -0,0 +1,31 @@
+import { type NextFunction, type Request, type Response } from "express";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import redis from "redis";
+
+import { AppError } from "@shared/errors/AppError";
+
+export default async function rateLimiter(
+    request: Request,
+    response: Response,
+    next: NextFunction
+): Promise<void> {
+    const redisClient = redis.createClient({
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+    });
+
+    const limiter = new RateLimiterRedis({
+        storeClient: redisClient,
+        keyPrefix: "rateLimiter",
+        points: 5,
+        duration: 5,
+    });
+    try {
+        await limiter.consume(request.ip);
+
+        next();
+        return;
+    } catch (err) {
+        throw new AppError("Too many requests", 429);
+    }
+}
