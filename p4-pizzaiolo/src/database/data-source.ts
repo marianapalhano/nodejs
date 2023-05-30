d02d1da7490d984d0282190eaacaf1966b640800
Mariana <marianaufc@gmail.com>
2023-05-30 16:18:11 -0300

    feat(pizzaiolo): updates data source with env variables

--- p4-pizzaiolo/src/database/data-source.ts
+++ p4-pizzaiolo/src/database/data-source.ts
@@ -1,14 +1,14 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: true,
+    type: process.env.TYPEORM_CONNECTION as any,
+    host: process.env.TYPEORM_HOST,
+    port: Number(process.env.TYPEORM_PORT),
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    synchronize: process.env.TYPEORM_SYNCHRONIZE as any,
+    logging: process.env.TYPEORM_LOGGING as any,
     entities: [],
     subscribers: [],
     migrations: [],
